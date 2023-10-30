7ac90af18a151cbf359bd096d631df23e712c68c
Christian <c.espinoza.code@gmail.com>
2023-10-30 16:40:27 -0500

    ejercicio de reactividad con watcher creado

--- src/components/Watcher.vue
+++ src/components/Watcher.vue
@@ -1,15 +1,31 @@
 <script setup lang="ts">
 import { ref, computed, watch } from 'vue';
   const texto = ref('Initial Text')
+  const open = ref(false);
   const firstName = 'Christian'
   const lastName = 'Espinoza'
+
+  const puertaLabel = ref('Puerta Cerrada')
+
   const fullName = computed(() => {
     return `${firstName} ${lastName}`
   })
 
   // Vigilar cambios en el texto
-  watch(texto, () => {
-    console.log('texto cambiado')
+  watch(texto, (newText, oldText) => {
+    console.log('texto cambiado', newText, oldText)
+  }, )
+
+  watch(open, (newValue) => {
+    if (newValue) {
+      puertaLabel.value = 'Puerta Abierta'
+    } else {
+      puertaLabel.value = 'Puerta Cerrada'
+    }
+  })
+
+  const computedLabel = computed(() => {
+    return open.value ? 'Cerrar' : 'Abrir'
   })
 </script>
 
@@ -18,6 +34,9 @@ import { ref, computed, watch } from 'vue';
   <!-- <input type="text" :value="texto" @input="inputChange"/> -->
   <input type="text" v-model="texto"/>
   <p>Full Name {{fullName}}</p>
+  <p>Ejercicio</p>
+  <p>{{puertaLabel}}</p>
+  <button @click="open = !open">{{computedLabel}}</button>
 </template>
 
 <style scoped>
