54770407281becd4fff22dc694afb902e9c4cdda
Christian <c.espinoza.code@gmail.com>
2023-10-30 17:53:18 -0500

    event example added

---
+++ src/components/UserEvents.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const submitEvent = (
+  // e: Event
+  ) => {
+  // e.preventDefault()
+  console.log('Enviado');
+}
+</script>
+
+<template>
+  <form @:submit.prevent="submitEvent">
+  <!-- <form v-on:submit="submitEvent"> -->
+    <h1>User Events</h1>
+    <p>Formulario</p>
+    <button>Submit</button>
+  </form>
+</template>
+
