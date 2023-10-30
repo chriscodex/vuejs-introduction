952f3a2ccca79de046c9b9187cc9d91a58bed12a
Christian <c.espinoza.code@gmail.com>
2023-10-30 17:52:58 -0500

    reactive input ejemplo agregado

---
+++ src/components/ReactiveInput.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+  const texto = ref('Initial Text')
+
+  const inputChange = (e: any) => {
+    texto.value = e.target.value
+  }
+</script>
+
+<template>
+  <div>
+    <p>{{texto}}</p>
+    <input type="text" :value="texto" @input="inputChange"/>
+    <input type="text" v-model="texto"/>
+  </div>
+
+</template>
+
+<style scoped>
+  input {
+    border: 1px solid #ccc;
+    margin-right: 10px;
+  }
+</style>
