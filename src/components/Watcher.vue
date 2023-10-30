afd8f761449ff924f04b39609e7b7daca0ad8615
Christian <c.espinoza.code@gmail.com>
2023-10-30 15:29:40 -0500

    watcher example added

---
+++ src/components/Watcher.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref, computed, watch } from 'vue';
+  const texto = ref('Initial Text')
+  const firstName = 'Christian'
+  const lastName = 'Espinoza'
+  const fullName = computed(() => {
+    return `${firstName} ${lastName}`
+  })
+
+  // Vigilar cambios en el texto
+  watch(texto, () => {
+    console.log('texto cambiado')
+  })
+</script>
+
+<template>
+  <p>{{texto}}</p>
+  <!-- <input type="text" :value="texto" @input="inputChange"/> -->
+  <input type="text" v-model="texto"/>
+  <p>Full Name {{fullName}}</p>
+</template>
+
+<style scoped>
+  input {
+    border: 1px solid #ccc;
+    margin-right: 10px;
+  }
+</style>
