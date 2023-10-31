cd7f83e05562e04b28b23ae143b8e45ac2bc5058
Christian <c.espinoza.code@gmail.com>
2023-10-31 11:20:59 -0500

    child component example created

---
+++ src/components/caseComponent/ChildComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="item">
+    <div>{{ props.post.title }}</div>
+    <p>{{ props.post.description }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps(['post']);
+</script>
+
+<style scoped>
+.item {
+  border: 1px solid white;
+  padding: 1px;
+}
+</style>
