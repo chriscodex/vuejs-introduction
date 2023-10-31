ecc2f17a7d5e0d13a63b5018b10504135a2ca0bc
Christian <c.espinoza.code@gmail.com>
2023-10-31 17:58:12 -0500

    slot example created

---
+++ src/components/slotExample/RaizComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <layout>
+    <template v-slot:header> Title xddd </template>
+    <template v-slot:content> Esta es una descripción </template>
+  </layout>
+</template>
+
+<script setup lang="ts">
+import layout from './Layout.vue';
+</script>
+
+<style scoped></style>
