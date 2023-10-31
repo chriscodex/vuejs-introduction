3360761deba9ab944ea6c863e842b8b4a0e9d373
Christian <c.espinoza.code@gmail.com>
2023-10-31 17:57:50 -0500

    layout example created

---
+++ src/components/slotExample/Layout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <slot name="header"></slot>
+  </div>
+  <div>
+    Intermedio
+  </div>
+  <div>
+    <slot name="content"></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
