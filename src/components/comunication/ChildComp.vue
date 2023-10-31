2a1a27e21ad56201d44aa0c34b58f5d39b8610ba
Christian <c.espinoza.code@gmail.com>
2023-10-31 18:58:16 -0500

    ChildComp for example of comunication created

---
+++ src/components/comunication/ChildComp.vue
@@ -0,0 +1,14 @@
+<template>
+  <li>{{ element }}</li>
+</template>
+
+<script setup lang="ts">
+const {} = defineProps({
+  // element: String,
+  element: {
+    default: 'default',
+  }
+});
+</script>
+
+<style scoped></style>
