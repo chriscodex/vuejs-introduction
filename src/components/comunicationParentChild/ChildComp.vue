ae75b020d4c0d2a77b0fe00d794793b4616e4b40
Christian <c.espinoza.code@gmail.com>
2023-10-31 19:02:53 -0500

    files renamed

---
+++ src/components/comunicationParentChild/ChildComp.vue
@@ -0,0 +1,14 @@
+<template>
+  <li>{{ element }}</li>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  // element: String,
+  element: {
+    default: 'default',
+  }
+});
+</script>
+
+<style scoped></style>
---
+++ src/components/comunicationParentChild/ParentComp.vue
@@ -0,0 +1,12 @@
+<template>
+  <ul>
+    <ChildComp v-for="(item, index) in lista" :key="index" :element="item" />
+  </ul>
+</template>
+
+<script setup lang="ts">
+import ChildComp from './ChildComp.vue';
+const lista = ['uno', 'dos', 'tres', 'cuatro', undefined];
+</script>
+
+<style scoped></style>
