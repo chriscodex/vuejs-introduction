55a4996f759641715544e8992f61ca1be00764d8
Christian <c.espinoza.code@gmail.com>
2023-10-31 20:50:35 -0500

    parentComponent created for comunication from child to parent

---
+++ src/components/comunicationChildParent/ParentComp.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <ChildComp
+      v-for="(item, index) in lista"
+      :key="index"
+      :element="item"
+      @removeElement="removeElement(index)"
+    />
+  </ul>
+</template>
+
+<script setup lang="ts">
+import ChildComp from './ChildComp.vue';
+import { ref } from 'vue';
+const lista = ref(['uno 1', 'dos 2', 'tres 3', 'cuatro 4', undefined]);
+
+const removeElement = (indexRemove) => {
+  const newLista = lista.value.filter((listElement, listIndex) => {
+    return listIndex !== indexRemove;
+  });
+  lista.value = [...newLista];
+};
+</script>
+
+<style scoped></style>
